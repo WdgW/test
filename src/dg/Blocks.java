185c581c59e93c8251a5618bb729b1f7957abd1d
GD <114802062+WdgW@users.noreply.github.com>
2024-02-25 19:45:42 +0800

    Update Blocks.java

--- src/dg/Blocks.java
+++ src/dg/Blocks.java
@@ -3,13 +3,18 @@
  */
 package dg;
 
+import mindustry.type.Category;
 import mindustry.world.Block;
 
 public class Blocks {
     public static Block base,top;
     public static void load(){
-        base = new Block("base"){};
-        top = new Block("top"){};
+        base = new Block("base"){{
+            this.category = Category.defense;
+        }};
+        top = new Block("top"){{
+            this.category = Category.defense;
+        }};
         BaseMultipleBlock.create(base,top);
     }
 }
